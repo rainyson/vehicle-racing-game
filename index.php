8ecee82743f5cb9e2e9e4bcf81e5b5808ace6ce6
Babak Shokouhi Pour <rainyson@gmail.com>
2024-03-04 13:04:46 +0330

    add comments and change VehiclesList namespace

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Rainyson\VehicleRacingGame\Classes\VehicleList;
 use Rainyson\VehicleRacingGame\Commands\RaceCommand;
-use Rainyson\VehicleRacingGame\VehicleList;
 use Symfony\Component\Console\Application;
 
+// Create a list of vehicles
 VehicleList::setVehiclesList();
 
+// Create an instance of application
 $application = new Application();
 
+// add race command
 $application->add(new RaceCommand());
 
+// Run the application with executing command
 $application->run();
